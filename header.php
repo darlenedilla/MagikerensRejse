4137b1ed9cba53d6c487ee149957b6d835595b3e
1221s <nann9135@edu.eal>
2019-04-02 09:28:54 +0200

    indsat titel både meta data og alm.

--- header.php
+++ header.php
@@ -7,6 +7,9 @@
     <meta name="keywords" content="Magi, Harry Potter, Magiske Dage Odense, Odense, Festival, Magisk, Rejse">
     <meta name="author" content="Magiske Dage Odense">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta title="Magikerens Rejse">
+
+    <title><<?php get_title() ?></title>
 
 <!-- favicon -->
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
