393e58337338966e2ffed3c4c429376078f8feb9
1221s <nann9135@edu.eal>
2019-04-03 13:08:24 +0200

    still working on footer header

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
     <div class="footerMenu">
       <?php wp_nav_menu( array('theme_location' => 'footer_menu') ); ?>
-      <img class="footerMenu-bg" src="<?php echo get_stylesheet_directory_uri(); ?>/img/footerMenu.png" alt="Background for the footer">
+      <img class="footerMenu-bg" src="<?php echo get_stylesheet_directory_uri(); ?>/img/footermenu.png" alt="Background for the footer">
     </div>
 
 </footer>
