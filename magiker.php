740a3e022388a0b7cf6fa37c0c65e326c67bbba7
darlenedilla <darlenedilla@hotmail.com>
2019-04-08 21:10:34 +0200

    Kamera access, magiker database fetch - ikke færdigt

--- magiker.php
+++ magiker.php
@@ -4,9 +4,43 @@
 Template Name: magiker
 */
 ?>
+<?php
+// Fetch user data from database
 
-<section class="mainsection">
-  <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/background.jpg" alt="background">
-</section>
+include("config.php");
+
+$query = "select user.magicalName, house.name, bloodTypeName from user, house, bloodType, pets where user.phoneNo = 28141151";
+
+foreach ($con->query($query) as $row) {
+    print  "\n";
+    print $row['name'] . "\n";
+    print $row['magicalName'] . "\n";
+    print $row['bloodTypeName'] . "\n";
+}
+
+
+?>
+
+<!--<section class="mainsection" >
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/background.jpg" alt="background">
+
+    <div class="profileData">
+        <img id="profileImg" src="<?php echo $image_src;  ?>" >
+        <h2 id="profileName"> <?php echo $name?></h2>
+        <img id="petImg" src="<?php echo $image_src;?>">
+        <ul>
+            <li class="badges"><?php echo $badge?></li>
+            <li class="badges"><?php echo $badge?></li>
+            <li class="badges"><?php echo $badge?></li>
+            <li class="badges"><?php echo $badge?></li>
+        </ul>'
+        <span><?php echo $retning?></span>
+        <h4>Oplysninger</h4>
+        <ul>
+            <li><?php echo $hus?></li>
+            <li><?php echo $blodtype?></li>
+        </ul>
+    </div>
+</section>-->
 
 <?php get_footer(); ?>
--- main.js
+++ main.js
@@ -20,3 +20,61 @@ burgermenu.addEventListener("click", function(){
 });
 
 
+
+
+
+//CAMERA ACCESS
+
+/*const vid = document.querySelector('video');
+navigator.mediaDevices.getUserMedia({video: true}) // request cam
+.then(stream => {
+  vid.srcObject = stream; // don't use createObjectURL(MediaStream)
+  return vid.play(); // returns a Promise
+})
+.then(()=>{ // enable the button
+  const btn = document.querySelector('button');
+  btn.disabled = false;
+  btn.onclick = e => {
+    takeASnap()
+  };
+});
+
+function takeASnap(){
+  const canvas = document.createElement('canvas'); // create a canvas
+  const ctx = canvas.getContext('2d'); // get its context
+  canvas.width = vid.videoWidth; // set its size to the one of the video
+  canvas.height = vid.videoHeight;
+  ctx.drawImage(vid, 0,0); // the video
+  return new Promise((res, rej)=>{
+    canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+  });
+}
+/*function download(blob){
+  // uses the <a download> to download a Blob
+  let a = document.createElement('a'); 
+  a.href = URL.createObjectURL(blob);
+  a.download = 'screenshot.jpg';
+  document.body.appendChild(a);
+  a.click();
+}*/
+
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
+
+  const constraints = {
+    video: true,
+  };
+
+  captureButton.addEventListener('click', () => {
+    // Draw the video frame to the canvas.
+    context.drawImage(player, 0, 0, canvas.width, canvas.height);
+  });
+
+  // Attach the video stream to the video element and autoplay.
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      player.srcObject = stream;
+    });
+
