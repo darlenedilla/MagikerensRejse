0afd340f13e0b5354d3784737d15dc9b960bc253
darlenedilla <darlenedilla@hotmail.com>
2019-04-08 09:12:44 +0200

    opretet forbindelse til databasen

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
 
 $host = "1221s.com.mysql.service.one.com"; /* Host name */
-$user = "1221s_com_portfolio"; /* User */
-$password = "mku72szz"; /* Password */
-$dbname = "1221s_com_portfolio"; /* Database name */
+$user = "1221s_com_magikerensrejse"; /* User */
+$password = "magikerensRejse:)"; /* Password */
+$dbname = "1221s_com_magikerensrejse"; /* Database name */
 
+// Create connection
 $con = mysqli_connect($host, $user, $password,$dbname);
+
 // Check connection
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
