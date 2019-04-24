da4e82b3c4e1b9043c84189e4c6d469ef5ca169e
darlenedilla <darlenedilla@hotmail.com>
2019-04-25 00:18:27 +0200

    Magiker profil

--- magiker.php
+++ magiker.php
@@ -13,89 +13,80 @@ include("config.php");
 // $password = "Dgs55qhk:).."; /* Password */
 // $dbname = "1221s_com_magikerensrejse"; /* Database name */
 
-// Create connection
-$con = mysqli_connect($host, $user, $password,$dbname);
+// //Create connection
+// $con = mysqli_connect($host, $user, $password,$dbname);
 
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+// // Check connection
+// if (!$con) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
 
 //HER HENTES SOM ER GEMT PÅ BRUGEREN MED TELEFONNUMMER 28141151-->
 //Hent billedet fra databasen
 $sql = "SELECT image FROM user WHERE phoneNo= 28141151";
 //Send query'en afsted
-$result = mysqli_query($con,$sql);
+$result1 = mysqli_query($con,$sql);
 //Lav et array med de resultater der kommer ud fra vores sql
-$row = mysqli_fetch_array($result);
+$row1 = mysqli_fetch_array($result1);
 
 //Tag image-attributten fra tabellen
-$imageName = $row['image'];
-$badgeImgName = $row['badgeImg'];
+$imageName = $row1['image'];
 
 
-$query = "SELECT user.magicalName, 
-            user.name AS username, 
-            house.name AS houseName, 
-            bloodType.bloodTypeName, 
-            user.image AS profileImg, 
-            pets.name AS petName, 
-            journey.name AS journeyName, 
-            badge.image AS badgeImg,
-            userBadge.badgeId as badgeId
-            FROM user, house, bloodType, journey, pets, userJourney, userBadge, badge
+$query = "SELECT user.magicalName, bloodtype.bloodTypeName, house.name AS houseName, pets.name AS petName, journey.name AS journeyName 
+            FROM user, bloodtype, house, pets, journey, userjourney
             WHERE user.phoneNo = 28141151 
-            AND userJourney.userId = user.phoneNo
-            AND userBadge.userId = user.phoneNo
-            AND badge.badgeId = userBadge.badgeId;";
-        $result = $con->query($query);
-        if($result->num_rows > 0){
+            AND bloodtype.bloodTypeId = user.bloodTypeId
+            AND house.houseId = user.houseId
+            AND pets.petId = user.petId
+            AND journey.journeyId = userjourney.journeyId
+            AND userjourney.userId = user.phoneNo";
+
+
+        $result2 = $con->query($query);
+
+        if($result2->num_rows > 0){
             //output data of each row
-            while($row = $result->fetch_assoc()){
+            while($row2 = $result2->fetch_assoc()){
                 //Variables to use in html
-            $image_src = get_stylesheet_directory_uri().'/img/' ."$imageName";
-            $badgeImage_src =  get_stylesheet_directory_uri().'/img/badges/' ."$badgeImageName";
-            $magicalName = $row['magicalName'];
-            $badgeId = $row['badgeId'];
-            $journeyName = $row['journeyName'];
-            $houseName = $row['houseName'];
-            $bloodTypeName = $row['bloodTypeName'];
-            $petName = $row['petName'];
+            $magicalName = $row2['magicalName'];
+            $journeyName = $row2['journeyName'];
+            $houseName = $row2['houseName'];
+            $bloodTypeName = $row2['bloodTypeName'];
+            $petName = $row2['petName'];
             }
         }
         else{
             echo "0 results";
-        }
+        };
 
-        $badgeQuery = "SELECT badge.image as badgeImage 
-        FROM badge, userbadge 
-        WHERE userbadge.userId = 28141151
-        AND userbadge.badgeId = badge.badgeId
-        AND userbadge.badgeId 
-        IN (1,2,3,4) 
-        GROUP BY badge.image;";
 
-         $result = $con->query($badgeQuery);
-         if($result->num_rows > 0){
+        $badgeQuery = "SELECT badge.image as badgeImg 
+                        FROM badge, userbadge 
+                        WHERE userbadge.userId = 28141151
+                        AND userbadge.badgeId = badge.badgeId
+                        AND userbadge.badgeId IN (1,2,3,4) 
+                        GROUP BY badge.image;";
+
+         $result3 = $con->query($badgeQuery);
+         if($result3->num_rows > 0){
              //output data of each row
-             while($row = $result->fetch_assoc()){
+             while($row3 = $result3->fetch_assoc()){
                  //Variables to use in html
-                 //echo $row['badgeImage'] .'</br>';
-             }
+                 //echo $row3['badgeImg'] .'</br>';
+                 $arrayOfBadges[$row3['badgeImg']][] = $result3;
+            }
          }
          else{
              echo "0 Badges found";
          }
-
-
-        $con->close();
 ?>
 
 
-<section class="mainsection" >
+<section class="mainsecti" >
     <div class="profileData">
         <div id="profileImgContainer">
-            <img id="profileImg" src="<?php echo $image_src;  ?>" >
+            <img id="profileImg" src="<?php echo get_stylesheet_directory_uri().'/img/portraits/' .$imageName;?>" >
             <div id="ribbonContainer">
                 <h3 id="magicalName"><?php echo $magicalName; ?></h3>
                 <img id="ribbon" src="<?php echo get_stylesheet_directory_uri();?>/img/ribbon.png" alt="Ribbon">
@@ -103,10 +94,13 @@ $query = "SELECT user.magicalName,
         </div>
         <!--<img id="petImg" src="">-->
         <ul id="listOfBadges">
-            <li class="badges"><img class="badgeImg" src="<?php echo get_stylesheet_directory_uri();?>/img/badges/wandBadge.png"></li>
-            <li class="badges"><img class="badgeImg" src="<?php echo get_stylesheet_directory_uri();?>/img/badges/capeBadge.png"></li>
-            <li class="badges"><img class="badgeImg" src="<?php echo get_stylesheet_directory_uri();?>/img/badges/ghostBadge.png"></li>
-            <li class="badges"><img class="badgeImg" src="<?php echo get_stylesheet_directory_uri();?>/img/badges/nordheimBadge.png"></li>
+        <?php 
+            foreach($arrayOfBadges as $badge_img => $group){
+                //Her looper vi igennem hver badge-image
+                $src = get_stylesheet_directory_uri() .'/img/badges/' .$badge_img;
+                echo("<li class='badges'><img class='badgeImg' src=$src></li>");
+            }
+        ?>
         </ul>
         <h4 id="journeyName"><?php echo $journeyName?></h4>
         <h4 id="otherInfo">Dine Oplysninger:</h4>
@@ -118,4 +112,26 @@ $query = "SELECT user.magicalName,
     </div>
 </section>
 <!--<img class="mainsectionImg" src="<?php echo get_stylesheet_directory_uri(); ?>/img/background.jpg" alt="background">-->
+
+<script>
+    var journeyName = "<?php echo $journeyName?>";
+    var profileImg = document.getElementById("profileImg");
+    switch(journeyName){
+        case "Magiker":
+            profileImg.style.borderColor = "#8d434c";
+            break;
+        case "Alkymist":
+            profileImg.style.borderColor = "#347f41";
+            break;
+        case "Quidditch Stjerne":
+            profileImg.style.borderColor = "#d5cb13";
+            break;
+        case "Dragetæmmer":
+            profileImg.style.borderColor = "#4a728c";
+            break;
+        case "Erfaren":
+            profileImg.style.borderColor = "#db873c";
+            break;
+    }
+</script>
 <?php get_footer(); ?>
