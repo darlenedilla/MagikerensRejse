cd5287b39f69e19ad0a57647198df09370f562a7
1221s <nann9135@edu.eal>
2019-05-07 12:05:05 +0200

    BADGESIDER FUNGERER AHHHHH :v:

--- badge-lb.php
+++ badge-lb.php
@@ -1,7 +1,45 @@
 <?php
+/*
+Template name: badge tester
+*/
+$server ="localhost";
+$user ="root";
+$pw ="";
+$db = "1221s_com_magikerensrejse";
+
+// Create connection
+$con = new mysqli($server, $user, $pw, $db);
+//check fann_get_total_connections
+if ($con->connect_error) {
+  die("Connection failed:" .$con->connect_error);
+} else {
+  echo '<script>console.log("connected succesfully")</script>';
+}
+
+  $user = 28141151; //SKAL LAVES OM SÅ DEN FINDER DEN BRUGER DER ER LOGGET IND!!!!
 
 
+//Fetching the badges this user has
+$selectBadgeSql = "SELECT badge.badgeId as badgeId
+              FROM badge, userbadge
+              WHERE userbadge.userId = 28141151
+              AND userbadge.badgeId = badge.badgeId
+              AND userbadge.badgeId IN (1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21)
+              GROUP BY badge.badgeId;";
+
+$selectBadgeQuery = $con->query($selectBadgeSql);
+if($selectBadgeQuery->num_rows > 0){
+   //output data of each row
+
+     $badgeArray = array();
+  while ($selectBadgeRow = $selectBadgeQuery->fetch_assoc()) {
+
+      foreach ($selectBadgeRow as $key => $value) {
+        array_push($badgeArray, $value);
+      }
+  }
 
 
+};
 
  ?>
